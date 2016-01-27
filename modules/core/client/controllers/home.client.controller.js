7ccc9657932c7281a6fb4afb1ea6fd7318e4e859
Tincho <tincho>
2016-01-26 21:46:56 -0300

    flaco

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
-    // This provides Authentication context.
-    $scope.authentication = Authentication;
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','$http','$interval',
+  function ($scope,Authentication, $http, $interval) {
+	// This provides Authentication context.
+	$scope.authentication = Authentication;
+	var url = '/modules/core/client/datasets/items.json';
+	$http.get(url).success(function(data){
+		$scope.lyrics = data;
+	});
+	$interval(function(){
+		$scope.random();
+	},6000);
+	$scope.random = function() {
+		return 0.5 - Math.random();
+    };
   }
 ]);
