7d9726e313daaa321181b2f087289ac33bc87f80
Tincho <tincho>
2016-02-08 20:16:14 -0300

    k1

---
+++ public/k1/README.md
@@ -0,0 +1,50 @@
+# Kaleidos: CSS Kaleidoscope Effects
+
+A simple kaleidoscope effect produced with a bunch of SASS-powered
+CSS rotation and scale transforms.
+
+See it in action here: http://coldhead.github.com/kaleidos/ or here
+http://gif.ly/
+
+**Suddenly, Convenience!** Add it to your browser here:
+https://github.com/miketaylr/kaleidos-extension/
+
+## Parameters
+You can pass parameters via the query string: e.g.
+?n=13&src=http://someurl/someimage.png
+
+* **n** is a positive integer representing the number of "segments" to
+create within the kaleidoscope. The provided CSS enthusiastically
+supports up to 32 segments but the effect works best with lower
+values.
+
+* **src** is a URL for an alternate image.
+
+* Assigning a truthy value to **clean** inhibits the display of the
+link to Github.
+
+## Dedication
+Dedicated to [@jeanettehayes](http://twitter.com/jeanettehayes), who I
+thought might like this kind of thing.
+
+## Minutiae
+The name "kaleidos" is derived from the Ancient Greek καλός (kalos),
+"beautiful, beauty" and εἶδος (eidos), "that which is seen: form,
+shape".
+
+This project is optimised for butterfly wings. A modern browser is
+mandatory. Happiness is mandatory.
+
+This might be more accurately described as a
+[teleidoscope](http://en.wikipedia.org/wiki/Teleidoscope) but who has
+ever heard of such a thing, really.
+
+## TODO
+* Some kind of build process like all the cool kids are doing.
+
+## Terms & Conditions
+
+This project is dual licenced under the MIT and GPL licenses because
+[that's what Cowboy said to
+do](http://benalman.com/news/2010/02/on-licensing-my-code/). I hope
+that's okay with everyone.
---
+++ public/k1/js/kaleidoscope.js
@@ -0,0 +1,173 @@
+$( document ).ready( function () {
+
+  function randomIntFromInterval(min,max)
+  {
+      return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  
+  var currentPhoto = '/k/img/k/flaco' + randomIntFromInterval(1,2) +'.jpg';
+  var parameters ={
+    s:5,
+    n:4,
+    src: currentPhoto,
+  }
+
+  var x = 0;
+  var y = 0;
+
+  var auto;
+  var auto_x = 0;
+  var auto_y = 0;
+  var auto_throttle;
+
+  // PARAMETER: *s* is the speed of the automatic timeout animation.
+  var s = parameters.s || 3;
+
+  // PARAMETER: *n* is the number of segments.
+  var n = ~~parameters.n || 7;
+  var tiles = '';
+  if ( n ) {
+    for ( var i = 0; i <= n * 2; i++ ) {
+      tiles += [ '<div class="tile t', i, '"><div class="image"></div></div>' ].join( '' );
+    }
+  }
+
+  var $kaleidescope = $( '.kaleidoscope' )
+    .addClass( 'n' + n )
+    .append( tiles );
+
+  var $image = $kaleidescope.find( '.image' );
+
+  var $fullscreen = $( '#fullscreen' );
+  var k = $kaleidescope[ 0 ];
+
+  // PARAMETER: *src* is the URL for an alternate image.
+  var src = parameters.src;
+  if ( src ) {
+    $image.css( 'background-image', [ 'url(', decodeURIComponent( src ), ')' ].join( '' ) );
+  }
+
+  // PARAMETER: *clean* hides the Github and fullscreen links.
+  var clean = parameters.clean;
+  if ( clean ) {
+    $( 'body' ).addClass('clean');
+  }
+
+  // PARAMETER: *opacity* sets opacity (0.0 -> 1.0).
+  var opacity = parseFloat( parameters.opacity );
+  if ( opacity ) {
+    $kaleidescope.css('opacity', 0).fadeTo( 3000, opacity );
+  }
+
+
+  // PARAMETER (undocumented): *mode* changes the animation style.
+  var mode = ~~parameters.mode || 2;
+
+  // Project changes in cursor (x, y) onto the image background position.
+  $kaleidescope.mousemove( function ( e ) {
+    x++;
+    y++;
+
+    var nx = e.pageX, ny = e.pageY;
+    switch ( mode ) {
+      case 1:
+        nx = -x;
+        ny = e.pageY;
+        break;
+      case 2:
+        nx = e.pageX;
+        ny = -y;
+        break;
+      case 3:
+        nx = x;
+        ny = e.pageY;
+        break;
+      case 4:
+        nx = e.pageX;
+        ny = y;
+        break;
+      case 5:
+        nx = x;
+        ny = y;
+        break;
+    }
+
+    move( nx, ny );
+    auto = auto_throttle = false;
+  });
+  
+  // An alternate image can be supplied via Dragon Drop.
+  if ( 'draggable' in document.createElement('b') && window.FileReader ) {
+    k.ondragenter = k.ondragover = function( e ) {
+      e.preventDefault();
+    };
+  
+    k.ondrop = function( e ) {
+      readFile( e.dataTransfer.files[0] );
+      e.preventDefault();
+    };
+  }
+  
+  function readFile( file ) {
+    var r = new FileReader();
+    if ( !file.type.match('image\/.*') ) {
+      return false;
+    }
+    
+    r.onload = function( e ) {
+      $image.css( 'background-image', [ 'url(', e.target.result, ')' ].join( '' ) );
+    };
+    
+    r.readAsDataURL( file );
+  } 
+
+  // Request Fullscreen for maximum LSD effect
+  $fullscreen.click( function() {
+    if ( document.fullscreenEnabled || document.mozFullScreenEnabled || 
+        document.webkitFullscreenEnabled ) {
+      if ( k.requestFullscreen )       k.requestFullscreen();
+      if ( k.mozRequestFullScreen )    k.mozRequestFullScreen();
+      if ( k.webkitRequestFullscreen ) k.webkitRequestFullscreen();
+    }
+  });
+
+  // Animate all the things!
+  window.requestAnimFrame = ( function( window ) {
+    var suffix = "equestAnimationFrame",
+      rAF = [ "r", "webkitR", "mozR" ].filter( function( val ) {
+        return val + suffix in window;
+      })[ 0 ] + suffix;
+
+    return window[ rAF ]  || function( callback ) {
+      window.setTimeout( function() {
+        callback( +new Date() );
+      }, 1000 / 60 );
+    };
+  })( window );
+
+  function animate() {
+    var time = new Date().getTime() * [ '.0000', s ].join( '' );
+    auto_x = Math.sin( time ) * document.body.clientWidth;
+    auto_y++;
+
+    move( auto_x, auto_y );
+    if ( auto ) requestAnimFrame( animate );
+  }
+
+  function move( x, y ) {
+    $image.css( 'background-position', [ x + "px", y + "px" ].join( ' ' ) );
+  }
+
+  // Timer to check for inactivity
+  (function timer() {
+      setTimeout( function() {
+        timer();
+        if( auto && !auto_throttle ) {
+          animate();
+          auto_throttle = true;
+        } else {
+          auto = true;
+        }
+      }, 5000 );
+  })();
+});
